fdc40cc8021aa96e7f314416b99ee99c382853ce
Hyde Lerma <shuyojl@gmail.com>
2017-04-05 18:32:09 -0500

    test for column schema validator: ColumnSchemaValidator should return an error when specific column doesnt exists

---
+++ test/fixtures/validator/column-schema-validator/output.js
@@ -0,0 +1,14 @@
+export default {
+  resultsWithError: {
+    error: {
+      name: "ColumnSchemaValidatorError",
+      details: {
+        message: "La columna 'RequiredColumnToTest' no existe en el archivo",
+        path: "RequiredColumnToTest"
+      }
+    }
+  },
+  results: {
+    error: null
+  }
+}
